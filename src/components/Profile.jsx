614a95407bbf05825ab64557332014bf1d5de014
Nikita <Nikitos1502@yandex.ru>
2020-06-03 02:12:08 +0700

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import App from "../App";
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://lh3.googleusercontent.com/proxy/hIefQGf0ikSnYMzDgwywEU3aKMgjfGLO9Ni8YzTzjyD7zdaGtI3ikdkvyloiBjISAman200Nijsdkf8ijRKn6CDMLnK7Y5u__qI"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>new post</div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+            {/*<div>*/}
+            {/*    <img src="https://cs11.pikabu.ru/post_img/big/2019/04/26/11/1556308444127379407.jpg"/>*/}
+            {/*</div>*/}
+        </div>
+    )
+}
+
+export default Content;
